ad0644c93c36858b2a28e712d1f12cf9927c5528
Raphael Amoedo <raphael.amoedo@olxbr.com>
2019-01-26 14:36:59 -0200

    Add logging on requests

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"strconv"
-	"time"
-
 	api "kube_features/api/handlers"
+	"os"
 
 	"net/http"
 
@@ -15,36 +10,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var Server *http.Server
-
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/health", api.HealthCheck).Methods("GET")
+	router.Handle("/health", handlers.CombinedLoggingHandler(os.Stdout, http.HandlerFunc(api.HealthCheck))).Methods("GET")
 
-	// Configure CORS (allow all origins, and some methods for now)
 	corsOrigins := handlers.AllowedOrigins([]string{"*"})
 	corsMethods := handlers.AllowedMethods([]string{"GET", "HEAD", "OPTIONS"})
 
 	h := handlers.CORS(corsOrigins, corsMethods)(router)
-	s := &http.Server{Addr: "0.0.0.0:" + strconv.Itoa(8081), Handler: h, ReadTimeout: 7 * time.Second, WriteTimeout: 7 * time.Second}
-	Server = s
 
-	// Run our server in a goroutine so that it doesn't block.
-	go func() {
-		if err := s.ListenAndServe(); err != nil {
-			//log.Fatalf("• Error: %s", err.Error())
-		}
-	}()
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-
-	<-c
-
-	// Create a deadline to wait for.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-
-	Server.Shutdown(ctx)
-	os.Exit(0)
+	http.ListenAndServe(":8081", h)
 }
